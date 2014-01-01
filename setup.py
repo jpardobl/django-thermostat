ab440300a9527463186bf9bd180d079afb8d258f
jpardobl <raton@acadama.(none)>
2014-01-01 18:59:23 +0100

    little improvemente in is_at_night mapping

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name = 'django-thermostat',
-    version = '2.0-alpha',
+    version = '2.0.1-alpha',
     packages = [],
     include_package_data = True,
     license = 'BSD License',
