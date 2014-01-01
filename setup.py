9964bea2040c95c833d969c8e8439a9018ed7774
jpardobl <raton@acadama.(none)>
2014-01-01 19:29:09 +0100

    fixed a bug in views

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name = 'django-thermostat',
-    version = '2.0.2-alpha',
+    version = '2.0.3-alpha',
     packages = [],
     include_package_data = True,
     license = 'BSD License',
