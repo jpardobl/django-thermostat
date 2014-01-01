d2d08b491a1ab2bac13d0b9315911308f702db6d
jpardobl <raton@acadama.(none)>
2014-01-01 13:49:30 +0100

    modified to adopt multiple thermometer

--- django_thermostat/mappings/weather.py
+++ django_thermostat/mappings/weather.py
@@ -1,18 +1,18 @@
 import requests
 from django_thermostat import settings
 from hautomation_restclient.cmds import pl_switch
-from django_thermostat.models import Context
+from django_thermostat.models import Context, Thermometer
 from django.core.urlresolvers import reverse
 from time import strftime, localtime
-import os
+import os, logging
 
 
 def current_internal_temperature(mo=None):
     try:
-        ret = requests.get(os.path.join(settings.INTERNAL_TEMPERATURE_URI))
-        return float(ret.json()["internal"])
+
+        return float(Thermometer.objects.get(is_internal_reference=True).read())
     except Exception, ex:
-        print "Ex eption %s" % ex
+        logging.error(ex)
         return None
 
 
@@ -39,7 +39,7 @@ def tuned_temperature(mo=None):
 
 def tune_to_confort(mo=None):
     #print ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Tunning to confort"
-
+    logging.debug("Tunning to confort")
     ctxt = Context.objects.get()
     ctxt.tuned_temperature = ctxt.confort_temperature
     ctxt.save()
@@ -47,6 +47,7 @@ def tune_to_confort(mo=None):
 
 def tune_to_economic(mo=None):
     #print ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Tuning to economic"
+    logging.debug("Tunning to economic")
     ctxt = Context.objects.get()
     ctxt.tuned_temperature = ctxt.economic_temperature
     ctxt.save()
@@ -90,7 +91,7 @@ def start_flame():
     ctxt = Context.objects.get()
     ctxt.flame = True
     ctxt.save()
-
+    logging.debug("Flame started")
     #print "%s flame started" % strftime("%d.%m.%Y %H:%M:%S", localtime())
 
 
@@ -108,6 +109,7 @@ def stop_flame():
     ctxt = Context.objects.get()
     ctxt.flame = False
     ctxt.save()
+    logging.debug("Flame stopped")
     #print "%s flame stopped" % strftime("%d.%m.%Y %H:%M:%S", localtime())
 
 
--- django_thermostat/rules.py
+++ django_thermostat/rules.py
@@ -1,9 +1,9 @@
-
 from django_thermostat.mappings import get_mappings
 from pypelib.RuleTable import RuleTable
 from django_thermostat.mappings import get_mappings
 from django_thermostat.utils import gen_comparing_time
 from django_thermostat.models import Rule
+import logging
 
 
 def evaluate_non_themp():
@@ -31,49 +31,40 @@ def evaluate():
 
     mappings = get_mappings()
     table = RuleTable(
-        "Calecfaccion Invierno laborables",
+        "Decide tunned temp",
         mappings,
         "RegexParser",
         #rawfile,
         "RAWFile",
         None)
 
-    #start_time = gen_comparing_time(10, 0, 0)
+    logging.debug("current time: %s " % mappings["current_time"]())
+    logging.debug("current day of week: %s" % mappings["current_day_of_week"]())
+    logging.debug("current temp %s" % mappings["current_internal_temperature"]())
+    logging.debug("economic %s" % mappings["economic_temperature"]())
+    logging.debug("confort %s" % mappings["confort_temperature"]())
+    logging.debug("tuned %s" % mappings["tuned_temperature"]())
+    logging.debug("flame %s" % mappings["flame_on"]())
+    logging.debug("heat on %s" % mappings["heater_on"]())
 
-    #end_time = gen_comparing_time(13, 0, 0)
-    #print " ############################## estar time: %s" % start_time
-    #print " ############################## end time: %s " % end_time
-    print " ############################## current time: %s " % mappings["current_time"]()
-    print " ############################## current day of week: %s" % mappings["current_day_of_week"]()
-    print " ############################## current temp %s" % mappings["current_internal_temperature"]()
-    print " ############################## economic %s" % mappings["economic_temperature"]()
-    print " ############################## confort %s" % mappings["confort_temperature"]()
-    print " ############################## tuned %s" % mappings["tuned_temperature"]()
-    print " ############################## flame %s" % mappings["flame_on"]()
-    print " ############################## heat on %s" % mappings["heater_on"]()
     table.setPolicy(False)
 
     table.addRule("if heater_manual = 1  then accept do tune_to_confort")
-#    table.addRule("if (heater_manual = 0 ) && (0 = is_weekend) && ((current_time > %f) && (current_time < %f)) then accept" % (start_time, end_time))
 
     for rule in Rule.objects.filter(active=True, thermostat=True).order_by("pk"):
         table.addRule(rule.to_pypelib())
 
- #   print "DUMP *******************************"
-    table.dump()
- #   print "END DUMP *******************************"
+    logging.debug("thermostat table %s" % table.dump())
 
     metaObj = {}
 
-    #Create the metaObj
     try:
         table.evaluate(metaObj)
-
     except Exception:
         mappings["tune_to_economic"]()
 
     table1 = RuleTable(
-        "Calecfaccion",
+        "Decide flame status",
         mappings,
         "RegexParser",
         #rawfile,
@@ -82,19 +73,15 @@ def evaluate():
     table1.addRule("if heater_on = 0 then deny")
     table1.addRule("if current_internal_temperature < tuned_temperature then accept")
 
-    print "DUMP *******************************"
-    table1.dump()
-    print "END DUM **********************************"
-
     try:
         table1.evaluate(metaObj)
         try:
             mappings["start_flame"]()
         except Exception, ex:
-            print "ERROR: Cant start flame: %s" % ex
+            logging.error("ERROR: Cant start flame: %s" % ex)
     except Exception as e:
         try:
             mappings["stop_flame"]()
         except Exception, ex:
-            print "ERROR: Cant stop flame: %s" % ex
+            logging.error("ERROR: Cant stop flame: %s" % ex)s
 
