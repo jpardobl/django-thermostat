45f5200d7ef72792e72936ea7ca5d24c254efa55
jpardobl <jpardo@digitalhigh.es>
2014-01-01 17:53:59 +0100

    changed version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name = 'django-thermostat',
-    version = '0.2',
+    version = '2.0-alpha',
     packages = [],
     include_package_data = True,
     license = 'BSD License',
