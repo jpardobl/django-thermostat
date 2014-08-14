48299c3db22ff387c4a58cb9531248bf9f3a3c73
jpardobl <kit2707888>
2014-08-14 13:22:34 +0200

    Added stats for temperature

--- django_thermostat/templates/therm/js/stats.js
+++ django_thermostat/templates/therm/js/stats.js
@@ -44,6 +44,24 @@ function load_stats(selector, grouping){
                           placement: 'inside',
                         fontSize: '11px'
                     } ,
+                highlighter: {
+                    lineWidthAdjust: 2.5,   // pixels to add to the size line stroking the data point marker
+                                            // when showing highlight.  Only affects non filled data point markers.
+                    sizeAdjust: 5,          // pixels to add to the size of filled markers when drawing highlight.
+                    showTooltip: true,      // show a tooltip with data point values.
+                    tooltipLocation: 'nw',  // location of tooltip: n, ne, e, se, s, sw, w, nw.
+                    fadeTooltip: true,      // use fade effect to show/hide tooltip.
+                    tooltipFadeSpeed: "fast"// slow, def, fast, or a number of milliseconds.
+                    tooltipOffset: 2,       // pixel offset of tooltip from the highlight.
+                    tooltipAxes: 'both',    // which axis values to display in the tooltip, x, y or both.
+                    tooltipSeparator: ', '  // separator between values in the tooltip.
+                    useAxesFormatters: true // use the same format string and formatters as used in the axes to
+                                            // display values in the tooltip.
+                    tooltipFormatString: '%.5P' // sprintf format string for the tooltip.  only used if
+                                                // useAxesFormatters is false.  Will use sprintf formatter with
+                                                // this string, not the axes formatters.
+                },
+
 
                 axes: {
                     // options for each axis are specified in seperate option objects.
