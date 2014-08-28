b4577bdc917d39a2232d95e1f50134b043e11dd0
jpardobl <kit2707888>
2014-08-28 16:29:04 +0200

    made button desapear

--- django_thermostat/pypelib/utils/Logger.py
+++ django_thermostat/pypelib/utils/Logger.py
@@ -1,6 +1,5 @@
 import logging
 from django.conf import settings
-#logging.basicConfig(format='%(asctime)s [%(filename)s:%(lineno)d] %(levelname)s: %(message)s', datefmt='%d/%m/%Y %I:%M:%S %p', level=logging.INFO)
 
 
 class Logger():
