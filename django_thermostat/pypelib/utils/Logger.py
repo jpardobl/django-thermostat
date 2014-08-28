54d45efafbc83cc1961104a6dd212a6a1f5d9032
jpardobl <kit2707888>
2014-08-28 16:30:45 +0200

    made button desapear

--- django_thermostat/pypelib/utils/Logger.py
+++ django_thermostat/pypelib/utils/Logger.py
@@ -4,9 +4,9 @@ from django.conf import settings
 
 class Logger():
 
-	@staticmethod
-	def getLogger():
-		#Simple wrapper. Ensures logging is always correctly configured (logging.basicConfig is executed)
-		logger = logging.getLogger("thermostat.rules")
+    @staticmethod
+    def getLogger():
+
+        logger = logging.getLogger("thermostat.rules")
         logger.setLevel(settings.LOG_LEVEL)
         return logger
