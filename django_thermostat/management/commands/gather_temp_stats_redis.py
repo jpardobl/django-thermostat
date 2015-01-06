0a7212d3f488efe871da6ed4a8458244603901a1
jpardobl <kit2707888>
2015-01-06 13:51:36 +0100

    added command gratherm_themp_stats_redis

---
+++ django_thermostat/management/commands/gather_temp_stats_redis.py
@@ -0,0 +1,50 @@
+from django.core.management.base import BaseCommand, CommandError
+from django_thermostat.models import ThermometerData, Thermometer
+#from django_thermostat import settings
+import requests
+from time import strftime, localtime
+from django.core.urlresolvers import reverse
+import simplejson
+import logging
+from django.conf import settings
+import multiprocessing, redis
+
+
+logger = logging.getLogger("thermostat.stats.redis")
+logger.setLevel(settings.LOG_LEVEL)
+
+class Command(BaseCommand):
+    args = ''
+    help = 'retrieve temperature stats and store into redis'
+
+    def __init__(self):
+	p = multiprocessing.current_process()
+        p.name = "temp_stats_redis"
+
+    def handle(self, *args, **options):
+        try:
+            logger.info("Starting at %s" % strftime("%d.%m.%Y %H:%M:%S", localtime()))
+            r = redis.Redis(settings.REDIS_GRADIENT_HOST)
+
+            sec = strftime("%d.%m.%Y %H:%M", localtime())
+
+            if not settings.LIST_THERMOMETERS_API is None:
+                ret = requests.get("http://localhost%stemperatures=True" % reverse("temperatures"))
+                therms = ret.json()
+            else:
+                therms = simplejson.loads(read_temperatures())
+
+            for therm in therms:
+                try:
+                    thermometro = Thermometer.objects.get(caption=therm)
+                    value = therms[therm]["temp"]["celsius"]
+                    logger.debug("Thermometer: %s; value: %s" % (therm, value))
+                    r.set("%s-%s" % (thermometro.caption, sec), value)
+                    #ThermometerData(thermometer=thermometro, value=value).save()
+                except Exception as ex:
+                    logger.error("Error gathering %s data: %s" % (therm, ex))
+                    continue
+            logger.info("Ended at %s" % strftime("%d.%m.%Y %H:%M:%S", localtime()))
+        except Exception as et:
+            self.stderr.write("Error gathering temp stats: %s" % et)
+            logger.error("Error gathering temp stats: %s" % et)
